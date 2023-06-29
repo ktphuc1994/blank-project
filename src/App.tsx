0ec1be272994aac7cc70f6369880e5ee8422e2e6
JacksonTran <jackson.tran@beyondedge.co>
2023-06-29 16:08:52 +0700

    add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
+import oceanPic from './assets/images/ocean.jpg';
 import './assets/styles/app.css';
 
 const App: FC = () => {
   return (
     <div>
       <h1>Bài viết được viết tại blog {process.env.HOST}</h1>
+      <img src={oceanPic} alt='Ocean Picture' width={100} height={100} />
     </div>
   );
 };
---
+++ src/types/env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    //types of envs
+    HOST: string;
+  }
+}
